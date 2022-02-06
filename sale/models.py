de2bc9663fb9618d68da2cd4bbb684d74eddf08f
gveitia13 <gveitia13@gmail.com>
2022-02-06 16:20:43 -0500

    modelo sale actualizado

--- sale/models.py
+++ sale/models.py
@@ -9,7 +9,7 @@ class Sale(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Precio')
     cost = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Costo')
     point_of_sale = models.CharField(max_length=900, verbose_name='Punto de venta')
-    hash = models.CharField(max_length=900, unique=True, default='asd')
+    hash = models.CharField(max_length=900, unique=True, null=True, blank=True)
     date_creation = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name='Fecha de creación')
     date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
 
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
 # Create your tests here.
+import sqlite3
+
+from sqlite3 import Error
+
+from goDjango.settings import BASE_DIR
+
+
+def sql_connection():
+    try:
+
+        con = sqlite3.connect(BASE_DIR / 'db.sqlite3')
+
+        return con
+
+    except Error:
+
+        print(Error)
+
+
+def sql_table(con):
+    cursorObj = con.cursor()
+
+    cursorObj.execute(
+        "CREATE TABLE sale_sale("
+        "id integer PRIMARY KEY,"
+        "name text,"
+        "price real, "
+        "cost real,"
+        "point_of_sale text,"
+        "hash text,"
+        "date_creation text,"
+        "date_updated text)"
+    )
+
+    con.commit()
+
+
+con = sql_connection()
+
+sql_table(con)
