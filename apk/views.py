93908b67ca6922c6edbd4b8800d4e1e7803bc323
gveitia13 <gveitia13@gmail.com>
2022-02-16 04:43:11 -0500

    redirige al admin

--- apk/views.py
+++ apk/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
 
 
 def Index(request):
-    return HttpResponse('<h1><a href=/admin site style="text-decoration: none">by GoDjango</a></h1> ')
-
+    # return HttpResponse('<h1><a href=/admin site style="text-decoration: none">by GoDjango</a></h1> ')
+    return redirect('/admin')
